6e4bc58fde6061b5919fa66c7f92a4bb773ed0a5
gryzus <gryzus24@gmail.com>
2021-10-31 18:46:08 +0100

    implement threading add progress bars and update messages revise default configuration add more user agents cleanup

--- etutor_extractor.py
+++ etutor_extractor.py
@@ -1,20 +1,70 @@
+from concurrent.futures import ThreadPoolExecutor
+import random
 from getpass import getpass
-from bs4 import BeautifulSoup
+from typing import NamedTuple
+
 import requests
+from bs4 import BeautifulSoup
 from requests.exceptions import ReadTimeout
 
 LOGIN_URL = 'https://www.etutor.pl/account/login'
 REPETITIONS_URL = 'https://www.etutor.pl/words/user-words'
 REPETITION_BLOCKS_URL = 'https://www.etutor.pl/words/user-words-editor-preview/'
-USER_AGENT = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64;rv:89.0)Gecko/20100101 Firefox/90.0'
-}
+USER_AGENTS = (
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64;rv:89.0)Gecko/20100101 Firefox/90.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.67',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'
+)
+
+
+class ProgressBar:
+    def __init__(self, message, no_of_items):
+        self.message = message
+        self.no_of_items = no_of_items
+        self.progress = []
+
+    def __enter__(self):
+        print(f'{self.message}  [{self.no_of_items * " "}]\r', end='')
+        return self
+
+    def __exit__(self, ex_type, ex_val, ex_tb):
+        print()
+
+    def advance(self, filling='#'):
+        self.progress.append(filling)
+        bar = "".join(self.progress) + (self.no_of_items - len(self.progress)) * " "
+        print(f'{self.message}  [{bar}]\r', end='')
+
+
+class RepetitionBlock(NamedTuple):
+    soup: BeautifulSoup
+    href: str
+
+
+def download_blocks(session, progress):
+    def downloaded(href):
+        session.headers.update({'User-Agent': random.choice(USER_AGENTS)})
+        response = session.get(REPETITION_BLOCKS_URL + href, timeout=10)
+        if response.status_code == 200:
+            progress.advance()
+            return RepetitionBlock(soup=BeautifulSoup(response.text, 'lxml'), href=href)
+
+        progress.advance('-')
+        return RepetitionBlock(soup=None, href=href)
+    return downloaded
 
 
 def get_custom_hrefs(href_names):
-    print('Wybierz bloki do przetworzenia:\n'
+    print('Wybierz bloki powtórek do pobrania:\n'
           '2,4,3 etc.\n')
-    for index, (href, hname) in enumerate(href_names, start=1):
+    for index, (_, hname) in enumerate(href_names, start=1):
         print(f'{str(index):4s}{hname}')
     print()
 
@@ -30,42 +80,44 @@ def get_custom_hrefs(href_names):
     return hrefs
 
 
+def print_config(config):
+    print('Załadowana konfiguracja:')
+    for option, val in config.items():
+        print(f'  {option:16s} : {val}')
+
+
 def load_config():
     try:
         with open('config.txt', 'r', encoding='utf-8') as conf:
             options_list = sorted(conf.readlines())
     except FileNotFoundError:
-        config = {
-            'block_order': 'from_top',
+        print('==> Brak pliku konfiguracyjnego.')
+        return {
+            'block_order': 'newest_first',
             'delimiter': 'tab',
             'field_order': '1,2,3,4',
             'ignore_sentences': 'false',
-            'repetition_order': 'oldest_first'
+            'max_workers': '4',
+            'repetition_order': 'newest_first'
         }
-        print('==> Brak pliku konfiguracyjnego\n'
-              'Załadowano domyślną konfigurację:')
-        for op, val in config.items():
-            print(f'  {op:16s} : {val}')
-        return config
 
-    print('Załadowana konfiguracja:')
     key_val = []
     for line in options_list:
         line = line.strip()
-        if not line or line.strip().startswith('#'):
+        if not line or line.startswith('#'):
             continue
 
         t = line.split('=', 1)
-        op, val = t[0].strip().lower(), t[-1].strip().lower()
-        key_val.append((op, val))
-        print(f'  {op:16s} : {val}')
+        option, val = t[0].strip().lower(), t[-1].strip().lower()
+        key_val.append((option, val))
     return dict(key_val)
 
 
 def main():
     config = load_config()
+    print_config(config)
 
-    print('\nZaloguj się do eTutor, aby kontynuuować\n')
+    print('\nZaloguj się do eTutor, aby kontynuować\n')
     auth_data = {
         'login': input('Nazwa użytkownika: '),
         'haslo': getpass('Hasło: ')
@@ -73,25 +125,25 @@ def main():
 
     print('Logowanie do eTutora...')
     with requests.Session() as ses:
-        ses.headers.update(USER_AGENT)
+        ses.headers.update({'User-Agent': random.choice(USER_AGENTS)})
         response = ses.post(LOGIN_URL, data=auth_data, timeout=10)
 
     if 'Zaloguj się - eTutor' in response.text:
         print('Logowanie nie powiodło się\n'
               'Nieprawidłowa nazwa użytkownika lub hasło')
         import time
-        time.sleep(2.5)
+        time.sleep(2)
         return None
 
     print('Zalogowano pomyślnie\n')
-    print('Pozyskuję bloki z powtórkami...')
+    print('Szukam powtórek...')
     response = ses.get(REPETITIONS_URL, timeout=10)
     soup = BeautifulSoup(response.text, 'lxml')
 
     hrefs = [h['href'].rsplit('/', 1)[-1]
              for h in soup.find_all('a', class_='listName')]
 
-    block_order = config.get('block_order', 'from_top')
+    block_order = config['block_order']
     if block_order.startswith('from_b'):
         hrefs = hrefs[::-1]
     elif block_order.startswith('old'):
@@ -99,13 +151,13 @@ def main():
     elif block_order.startswith('new'):
         hrefs = sorted(hrefs, reverse=True)
     elif block_order.startswith('c'):
-        full_block_info = soup.find_all('td', class_='wordsListName')
-        block_info = [x.text.replace('\n', '').replace('\r', '').rsplit(')', 1)[0] + ')'
-                      for x in full_block_info]
-        block_info = [' '.join(x.split()) for x in block_info]
-        
-        href_names = tuple(zip(hrefs, block_info))
-        hrefs = get_custom_hrefs(href_names)
+        block_info = [
+            ' '.join(
+                (x.text.replace('\n', '').replace('\r', '').rsplit(')', 1)[0] + ')').split()
+            )
+            for x in soup.find_all('td', class_='wordsListName')
+        ]
+        hrefs = get_custom_hrefs(tuple(zip(hrefs, block_info)))
     else:  # from_top
         hrefs = hrefs
 
@@ -113,50 +165,70 @@ def main():
     new_first = config['repetition_order'].startswith('new')
     ignore_sentences = config['ignore_sentences'] == 'true'
     field_order = config['field_order'].split(',')
+    no_of_hrefs = len(hrefs)
 
-    print('Otwieram plik "karty.txt"\n')
-    with open('karty.txt', 'a', encoding='utf-8') as f:
-        for href in hrefs:
-            print(f'Otwieram blok {href}')
-            response = ses.get(REPETITION_BLOCKS_URL + href, timeout=10)
+    # eTutor doesn't tolerate more than 6 workers, using 4 as default just to be safe
+    with \
+        ProgressBar('Pobieram powtórki     ', no_of_hrefs) as progress, \
+        ThreadPoolExecutor(max_workers=int(config['max_workers'])) as executor \
+            :
+        dl_func = download_blocks(ses, progress)
+        blocks = executor.map(dl_func, hrefs)
 
-            print(f'Przetwarzam...')
-            block_soup = BeautifulSoup(response.text, 'lxml')
+    dl_error = False
+    with \
+        ProgressBar('Przetwarzam i zapisuję', no_of_hrefs) as progress, \
+        open('karty.txt', 'a', encoding='utf-8') as f \
+            :
+        for block in blocks:
+            if block.soup is None:
+                dl_error = True
+                progress.advance('-')
+                continue
+
             if ignore_sentences:
-                block = block_soup.find_all('p', class_='hws phraseEntity')
+                repetitions = block.soup.find_all('p', class_='hws phraseEntity')
             else:
-                block = block_soup.select('p.hws.phraseEntity, div.maintext')
+                repetitions = block.soup.select('p.hws.phraseEntity, div.maintext')
 
             processed = []
-            for repetition in block:
-                # processing with split and replace is much more efficient
-                # than using bs4.find on each div inside the block
-                extract = (repetition.text.strip())[:-11].replace('\n', '')
+            for repetition in repetitions:
+                # processing with split and replace is much
+                # more efficient than using bs4 find methods
+                extract = repetition.text.strip()[:-11].replace('\n', '')
 
                 phrase = extract.split('=', 1)[0].strip(', ').replace(' , ', ', ')
-                translation = (extract.split('=', 1)[-1].split('\xa0', 1)[0]).strip()
+                translation = extract.split('=', 1)[-1].split('\xa0', 1)[0]
 
-                synonyms = ''
                 if '\xa0synonym:' in extract:
                     synonyms = extract.split('\xa0synonym:')[-1].split('\xa0\r')[0]
-                    synonyms = synonyms.strip()
+                else:
+                    synonyms = ''
 
-                note = ''
                 if 'Edit the note' in extract:
-                    note = extract.rsplit('Edit the note', 1)[-1].strip()
+                    note = extract.rsplit('Edit the note', 1)[-1]
+                else:
+                    note = ''
 
-                data = {'0': '', '1': translation, '2': phrase, '3': synonyms, '4': note}
-                processed.append(connector.join([data[x] for x in field_order]) + '\n')
+                data = {'0': '',
+                        '1': translation,
+                        '2': phrase,
+                        '3': synonyms,
+                        '4': note}
+                processed.append(connector.join([data[x].strip() for x in field_order]) + '\n')
 
             if new_first:
-                processed = processed[::-1]
+                f.writelines(processed[::-1])
+            else:
+                f.writelines(processed)
 
-            print('Zapisuję...')
-            f.writelines(processed)
-            print(f'Blok {href} gotowy\n')
-            
-    print('Zakończono\n'
-          'Przetworzone karty zapisane do pliku "karty.txt"')
+            progress.advance()
+
+    if dl_error:
+        print('** Nie udało się pobrać wszystkich powtórek **\n'
+              '** Zmniejsz ilość wątków używanych do pobierania **')
+    else:
+        print('** Zakończono - karty zapisane do pliku "karty.txt" **')
 
 
 if __name__ == '__main__':
@@ -164,5 +236,5 @@ if __name__ == '__main__':
         raise SystemExit(main())
     except (KeyboardInterrupt, EOFError):
         print('\nUnicestwiony')
-    except ReadTimeout as e:
-        print('\nThe server did not send any data in the allotted amount of time:')
+    except ReadTimeout:
+        print('\nThe server did not send any data in the allotted amount of time')
